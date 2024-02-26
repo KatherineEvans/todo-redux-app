7bc2711b4757e334602bef89e6aae0c11ad1c661
Katherine Evans <kevans@neonone.com>
2024-02-25 19:32:41 -0600

    adds todos slice to app

---
+++ src/store/slice.jsx
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    { id: 1, text: "Feed Tiger dinner.", status: "incomplete" },
+    { id: 2, text: "Clean out fridge.", status: "incomplete" },
+    { id: 3, text: "Do laundry", status: "incomplete" },
+  ],
+  appTitle: "Katherine's Todo List",
+};
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addToTodos: (state, action) => {
+      const newTodo = action.payload;
+      state.todos.push(newTodo);
+    },
+  },
+});
