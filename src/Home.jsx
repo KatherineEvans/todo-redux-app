cd821b2042df3b711a3da6a434296e22e52ce82a
Katherine Evans <kevans@neonone.com>
2024-02-25 19:53:25 -0600

    pull app name from the store

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,11 @@
 import { Todos } from "./Todos";
+import { useSelector } from "react-redux";
+
 export function Home() {
+  const title = useSelector((state) => state.todos.appTitle);
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <h1>{title}</h1>
       <Todos></Todos>
     </div>
   );
